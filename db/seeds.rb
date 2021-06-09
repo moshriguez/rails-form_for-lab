a44f846756824f8263ad5f18a1c8748ed3a68d43
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-08 23:16:50 -0700

    commit tree 23a00de7e7ea24940bbd503359af5cb82c565efa

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Waiyee', last_name: 'Hui')
+Student.create(first_name: 'Marc', last_name: 'Rodriguez')
+Student.create(first_name: 'Andrea', last_name: 'Kelso')
+
+SchoolClass.create(title: 'Intro to Cooking', room_number: 435)
+SchoolClass.create(title: 'Algebra III', room_number: 106)
+SchoolClass.create(title: 'Computer Science', room_number: 321)
